73e909563bf11af77f6f6904774b3b5da44726ee
irvin garcia <crossball29@gmail.com>
2022-09-12 08:16:05 -0500

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+require 'kramdown'
+
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::RickAndMorty.character,
+    content: Faker::TvShows::RickAndMorty.quote
+  )
+  article.save!
+end
